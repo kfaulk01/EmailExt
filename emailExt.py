7252ebb4c7a34532a214ade18ed57328bd1a0ca5
Krystal Faulk <57131088+kfaulk01@users.noreply.github.com>
2023-05-17 15:12:28 -0500

    Follow redirects to get the target link and title

--- emailExt.py
+++ emailExt.py
@@ -2,10 +2,10 @@
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import email
-import pandas
 from bs4 import BeautifulSoup
 import base64
+import re
+import requests
 
 # Define the scopes for the Gmail API
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -22,6 +22,7 @@ mailSvc = build('gmail', 'v1', credentials=creds)
 results = mailSvc.users().messages().list(userId='me', q='label:extraction').execute()
 messages = results.get('messages', [])
 
+# Get the sender, date, and subject from the email headers for each email
 for _ in messages:
     msg = mailSvc.users().messages().get(userId='me', id=_['id']).execute()
     headers = msg['payload']['headers']
@@ -32,11 +33,22 @@ for _ in messages:
     subject = [header['value'] for header in headers if header['name'] == 'Subject'][0]
     date = [header['value'] for header in headers if header['name'] == 'Date'][0]
     sender = [header['value'] for header in headers if header['name'] == 'From'][0]
-    
+
+    # Decodes the email body for each email
     for _ in msg['payload']['parts']:
         if _["mimeType"] in ["text/plain", "text/html"]:
             content = base64.urlsafe_b64decode(_["body"]["data"]).decode("utf-8")
+            # Next: Convert body to plain text
             
+            # Finds all links, excluding email addresses and phone numbers
             soup = BeautifulSoup(content, 'html.parser')
-            print(soup)
+            for _ in soup.find_all('a', href=re.compile(r'^(?!mailto:)(?!tel:)(?!sms:).*')):
+                url = _.get('href')
+
+                # Follows redirects to get target URL and title
+                response = requests.get(url, allow_redirects=True)
+                response_soup = BeautifulSoup(response.text, 'html.parser')
+                link_url = response.url
+                title = response_soup.title.string if response_soup.title else _.string
             
+                # DATABASE PORTION STARTS HERE
